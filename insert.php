98ac029d1ccdbd485e6758c24447ea3f5f4dc925
SURAJ KUMAR SHARMA <surajkumarsharma62@gmail.com>
2022-08-27 12:18:10 +0530

    some issue line_no_29 :send data into google sheet : result : cant send

--- insert.php
+++ insert.php
@@ -1,41 +1,70 @@
 <?php
 echo '<pre>';
+include '<script src="https://code.jquery.com/jquery-3.6.1.min.js" ></script>';
 
 $conn = mysqli_connect('localhost', 'root', '', 'test');
 
 
 if (isset($_POST['submit']) && $_POST['g-recaptcha-response']) {
-    
+
     $secret_key = "6LfbKqkhAAAAAM4CTdJ0rgYMbPC7nRme-HpQ_OlV";
     $response = $_POST['g-recaptcha-response'];
     $ip = $_SERVER['REMOTE_ADDR'];
     $url = "https://www.google.com/recaptcha/api/siteverify?secret={$secret_key}&response={$response}&ip={$ip}";
     $res =  file_get_contents($url);
     $data = json_decode($res, true);
- 
-    if($data['success']){
-        
+
+    if ($data['success']) {
+
         $name = $_POST['name'];
         $email = $_POST['email'];
         $mobile = $_POST['mobile'];
         $massage = $_POST['massage'];
         $address = $_POST['address'];
+// send data into database 
+        mysqli_query($conn, "INSERT INTO `data_table`( `name`, `email`, `mobile`, `massage`, `address`)
+                                VALUES ('$name','$email','$mobile','$massage','$address')");
+
+
+// send data into google sheet
+?>
+
+        <script>
+        $(document).ready(function(){
+            var obj =  {
+                            name: '<?php $name?>',
+                            email: '<?php $email?>',
+                            mobile: '<?php $mobile?>',
+                            massage: '<?php $massage?>',
+                            address: '<?php $address?>'
+                         
+                        }   
+            jQuery.ajax({
+                        url: 'https://script.google.com/macros/s/AKfycbwh8ovCsFwMzIVCPhfsu_u_JkUQPVo4d_muIVwOVUYxSf0c51TZ_6pwogKO5UHneTJJpA/exec',
+                        type: 'post',
+                        data:jQuery(obj).serializeArray(),
+                        success:function(res){
+                            alert(res);
+                        }
+                    });
+        });     
+        </script>
+    <?php
+
+    //  send Email 
+
         
-            $SQL ="INSERT INTO `data_table`( `name`, `email`, `mobile`, `massage`, `address`)
-                                 VALUES ('$name','$email','$mobile','$massage','$address')";
-            mysqli_query($conn,$SQL);
 
 
-     }else{
+    } else {
         echo "Error captcha ";
-     }
-
-} else{
+    }
+} else {
     ?>
     <script>
         alert("Please verify Captcha");
     </script>
-    <?php
+<?php
 }
 
 header("Location:index.php");
